22018003b333058e8bb654dde0f4f3562f8839d0
abdulmajeedrao303-sudo <abdulmajeedrao303@gmail.com>
2026-06-27 22:16:49 +0500

    CHAPTER 1

---
+++ index.js
@@ -0,0 +1,80 @@
+Q1: "Write a script to greet your website visitor using JS alert box?"
+Ans: alert("Welcome to our website")
+
+Q2: "Write a script to display following message on your web page:Error!Please enter a valid password?"
+Ans: alert("Error!Please enter a valid password.")
+
+Q3: "Write a script to display following message on your web page:(Hint:Use line break) Welcome to JS land...Happy Coding!"
+Ans:alert("Welcome to JS land...")
+   alert("Happy Coding!")
+
+Q4: "Write a script to display following messages in sequence:Welcome to JS land... Happy Coding!?"
+Ans: alert("Welcome to JS land...")
+     alert("Happy Coding!")
+
+Q5:"Generate the following message through browser's developer console:Hello...I can run JS through my web browser's console?"
+Ans: alert("Hello...I can run JS through my web browser's console") 
+
+Q6: "Make use of alerts in your new/existing HTML & CSS project?"
+Ans: "<h1>( My Project)</h1>"      
+<script>
+     alert("Welcome To My Project")
+    
+     Q7: "Question 7:Practice placement of <script></script> element in following sections of your project in exercise 6:" 
+        a.head
+        b.body("before your page's HTML")
+        c.body("inside your page's HTML")
+        d.body("after your page's HTML")
+Ans:
+(a)=<head>
+     <script>
+        alert("Welcome to my project")
+     </script>
+</head>
+(b)=<body>
+   
+    <script>
+        alert("Welcome to my project")
+    </script>
+
+    <h1>My project first page</h1>
+
+</body>
+(c)=<body>
+
+   <h1>Hello</h1>
+
+    <script>
+        alert("Welcome to my project")
+    </script>
+
+    <h2>My project first page</h2>
+    
+</body>
+(d)=<body>
+
+   <h1>Hello</h1>
+   <h2>Welcome</h2>
+
+    <script>
+        alert("Welcome to my project")
+    </script>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
