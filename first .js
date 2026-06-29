87b764cfd741bd9b4b5b93ebffd0d9b94dd950aa
abdulmajeedrao303-sudo <abdulmajeedrao303@gmail.com>
2026-06-29 12:33:06 +0500

    CHAPTER 4

---
+++ first .js
@@ -0,0 +1,31 @@
+Q1: "Declare 3 variables in one statement."
+Ans: var name="Abdul Samad";
+     var age="14";
+     var city="Karachi";
+
+Q2: "Declare 5 legal & 5 illegal variable names."
+Ans:"Legel variable:"
+ 1 ('var myName;')
+ 2 ('var age14')
+ 3 ('var _price')
+ 4 ('var $salary')
+ 5 ('var userName')
+
+  "Illegal variable:"
+ 1 ('var 3name')
+ 2 ('var my-name')
+ 3 ('var user name')
+ 4 ('var var')
+ 5 ('var @email')
+
+Q3: "Display this in your browser:"
+ a: ('A heading stating “Rules for naming JS variables')
+Ans: 'Rules for naming JS variables'
+ b: ('Variable names can only contain ______, ______,______ and ______.For example $my_1stVariable')
+ Ans: 'numbers,$,_and letters'
+c: ('Variables must begin with a ______, ______ or_____. For example $name, _name or name')
+Ans: 'letter, $ or _'     
+d: ('Variable names are case _________.')
+Ans: 'sensitive'     
+e: ('Variable names should not be JS _________.')
+Ans:'keywords'
