0fdc7e0274787c12f5383e4d5b2b4052b70f6210
abdulmajeedrao303-sudo <abdulmajeedrao303@gmail.com>
2026-06-29 12:13:03 +0500

    CHAPTER 3

---
+++ style.js
@@ -0,0 +1,47 @@
+Q1:"Declare a variable called age & assign it to your age.Show your age in an alert box"
+                    'I am 15 years old.'
+Ans: var age=15;
+    alert("I am " + age + "years old");
+
+Q2:"Declare & initialize a variable to keep track of how many times a visitor has visited a web page.For example:'You have visited this site N times'."
+                                     'You have visited this site 14 times'
+Ans: var visitcount=14;
+   alert("You have visited this site" + visitcount + "times");
+
+Q3:"Declare a variable called birthYear & assign it to your birth year.Show the following message in your browser:"
+               'My birth year is 1990'
+             'Data type of my declared variable is number'
+Ans: var birthYear = 1990;
+ document.write("My birth year is" + birthYear + "<br>");
+ document.write("Data type of my declared variable is" + typeof birthYear);
+
+Q4:"4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:"
+a: ("Visitor's name")
+b: ("Product title")
+c: ("Quantity i.e. how many products a visitor wants to order")
+"Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”."
+        'John Doe ordered 5 T-shirt(s) on XYZ Clothing store'
+Ans: var visitorName = "John Doe"
+     var productTitle = "T-shirt"
+     var quantity = 5 ;
+
+    document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
